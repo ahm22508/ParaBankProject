58dfb7a724f3c0e8c8fcf1b847ad5f06defa44ec
ahm22508 <ahmadamr659@gmail.com>
2024-04-13 22:31:19 +0200

    commit

--- src/main/java/ParaBankPages/NewAccountPage.java
+++ src/main/java/ParaBankPages/NewAccountPage.java
@@ -3,6 +3,7 @@ package ParaBankPages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -14,13 +15,16 @@ public class NewAccountPage {
         this.driver = driver;
     }
     private final By OpenNewAccountButton = By.xpath("//input[@type='submit']");
-    private final By SavingSelection = By.xpath("//option[text()='SAVINGS']");
-    private final By IdAccount = By.xpath("//option[text()='20004']");
+    private final By SavingSelection = By.id("type");
+    private final By IdAccount = By.id("fromAccountId");
 
     public void OpenNewAccount(){
-        driver.findElement(SavingSelection).click();
+        Select select = new Select(driver.findElement(SavingSelection));
+        select.selectByIndex(1);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(IdAccount));
+        Select select1 = new Select(driver.findElement(IdAccount));
+        select1.selectByIndex(2);
         driver.findElement(OpenNewAccountButton).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(FinishPage.GetAccountOpened()));
     }
